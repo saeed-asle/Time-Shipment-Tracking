b5ad417d656fc8b46da6cf1a92fa44204f5c1f05
saeed asle <saaedasle1@gmail.com>
2025-05-28 16:03:37 +0300

    Initial upload

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+routers=require('')
+
+
+const app = express();
+
+app.get('/',(req,res)=>res.send('Hellow'));
+app.listen(3000,()=>console.log('server started on port 3000'))
